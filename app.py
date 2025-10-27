6fdaa49b8d68911eed7647de5202bdca30af4582
lasya943 <lnalagandla@gmail.com>
2025-10-27 13:47:44 +0530

    first committtt

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def submit():
     gender = request.form.get('gender')
     return render_template('greeting.html',name = uname,dob = dob,gender = gender)
 if(__name__ == "__main__"):
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
